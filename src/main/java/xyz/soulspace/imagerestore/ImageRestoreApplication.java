8f6332ab46b17fec7b5daa92ba423bdc92fdb270
LiSoulSpace <lisoulspace@163.com>
2022-12-03 09:34:07 +0800

    init springboot

---
+++ src/main/java/xyz/soulspace/imagerestore/ImageRestoreApplication.java
@@ -0,0 +1,13 @@
+package xyz.soulspace.imagerestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/soulspace/imagerestore/ImageRestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.soulspace.imagerestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
